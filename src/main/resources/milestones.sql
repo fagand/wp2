829accd2be022c7e12c8789d436a1a93e522edcb
aaronamjid <38182559+aaronamjid@users.noreply.github.com>
2019-04-29 11:14:32 +0100

    milestones SQL file returned to full design, including foreign key.

--- src/main/resources/milestones.sql
+++ src/main/resources/milestones.sql
@@ -1,5 +1,10 @@
-CREATE TABLE IF NOT EXISTS milestones (
+ CREATE TABLE IF NOT EXISTS milestones (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
   msname VARCHAR(255),
   msdesc VARCHAR(255),
-  msduedate VARCHAR(255)
+  msduedate DATE(3),
+  compdate DATE(3),
+  userid INTEGER(255),
+  FOREIGN KEY(userid)
+  REFERENCES person (id)
 );
